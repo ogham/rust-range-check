0aecb8f9d079fd37b1bec5bd6956fd11a261b69d
Ben S <ogham@bsago.me>
2016-02-17 17:56:47 +0000

    Return debug info in the errors

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,17 @@ use std::result;
 pub trait RangeExt: Sized {
 
     /// Returns whether this value exists within the given range of values.
-    fn is_within(&self, range: Range<Self>) -> bool;
+    fn is_within(&self, range: &Range<Self>) -> bool;
 
     fn check_range(self, range: Range<Self>) -> Result<Self> {
-        if self.is_within(range) {
+        if self.is_within(&range) {
             Ok(self)
         }
         else {
-            Err(Error)
+            Err(Error {
+                allowed_range: range,
+                outside_value: self,
+            })
         }
     }
 }
@@ -25,13 +28,16 @@ pub trait RangeExt: Sized {
 // really ever used for numeric ranges...
 
 impl<T> RangeExt for T where T: PartialOrd<T> {
-    fn is_within(&self, range: Range<Self>) -> bool {
+    fn is_within(&self, range: &Range<Self>) -> bool {
         *self >= range.start && *self < range.end
     }
 }
 
 
-#[derive(PartialEq, Debug, Copy, Clone)]
-pub struct Error;
+#[derive(PartialEq, Debug, Clone)]
+pub struct Error<T> {
+    allowed_range: Range<T>,
+    outside_value: T,
+}
 
-pub type Result<T> = result::Result<T, Error>;
+pub type Result<T> = result::Result<T, Error<T>>;
